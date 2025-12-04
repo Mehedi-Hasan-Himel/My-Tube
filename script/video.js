85c6deb49d39a996b8042db3be95be74309cb6f3
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-12-04 13:49:47 +0600

    Access Object And Display Dynamic Data To Card

--- script/video.js
+++ script/video.js
@@ -44,17 +44,29 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card w-80";
     card.innerHTML = `
-   <figure>
+   <figure class="h-[200px] overflow-hidden">
     <img
     class="object-cover w-full h-48 rounded"
       src=${video.thumbnail}
-      alt="Shoes" />
+      alt=${video.title}
+      />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+      <img
+        class="w-10 h-10 rounded-full object-cover"
+        src=${video.authors[0].profile_picture}
+        alt=''
+      />
+      <div>
+        <h2 class="card-title text-lg font-semibold">${video.title}</h2>
+        <div class="flex items-center gap-2">
+            <p class="text-gray-400">${video.authors[0].profile_name}</p>
+            <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>
+        </div>
+        <p class="text-sm text-gray-500">${video.channel}</p>
+        <p class="text-sm text-gray-500">${video.views} views • ${video.date}</p>
+      </div>
     </div>
   </div>
     `;
